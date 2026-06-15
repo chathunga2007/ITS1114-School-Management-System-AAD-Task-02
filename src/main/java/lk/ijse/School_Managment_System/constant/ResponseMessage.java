9ba80a99c44a6779930ef391af830ec0544441ba
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-06-15 13:07:37 +0530

    constant added!

---
+++ src/main/java/lk/ijse/School_Managment_System/constant/ResponseMessage.java
@@ -0,0 +1,6 @@
+package lk.ijse.School_Managment_System.constant;
+
+public class ResponseMessage {
+    public static String SUCCESS_MESSAGE = "Operation Successful...";
+    public static String ERROR_MESSAGE = "Operation Failed...";
+}
---
+++ src/main/java/lk/ijse/School_Managment_System/constant/ResponseStatusCode.java
@@ -0,0 +1,6 @@
+package lk.ijse.School_Managment_System.constant;
+
+public class ResponseStatusCode {
+    public static int  OPERATION_SUCCESS = 0;
+    public static int  OPERATION_FAIL = 1;
+}
