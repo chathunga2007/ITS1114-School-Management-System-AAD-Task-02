ced8ff544ec6ee5aa479c2110aa17024faa9702d
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-06-15 13:07:21 +0530

    repo updated !

--- src/main/java/lk/ijse/School_Managment_System/repository/RecordRepository.java
+++ src/main/java/lk/ijse/School_Managment_System/repository/RecordRepository.java
@@ -1,5 +1,6 @@
 package lk.ijse.School_Managment_System.repository;
 
+import lk.ijse.School_Managment_System.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
