5a787b255a5da64f4f866e723f14075615c70601
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-06-15 13:11:04 +0530

    dto added!

---
+++ src/main/java/lk/ijse/School_Managment_System/dto/SectionDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.School_Managment_System.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SectionDTO {
+    private long sectionId;
+    private String description;
+}
--- src/main/java/lk/ijse/School_Managment_System/dto/StudentDTO.java
+++ src/main/java/lk/ijse/School_Managment_System/dto/StudentDTO.java
@@ -1,4 +1,25 @@
 package lk.ijse.School_Managment_System.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lk.ijse.School_Managment_System.enumeration.StudentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDTO {
+    private long studentId;
+    private String studentName;
+    private LocalDate dob;
+    @Enumerated(EnumType.STRING)
+    private StudentStatus studentStatus;
+
+    public StudentDTO(String studentName, LocalDate dob) {
+        this.studentName = studentName;
+        this.dob = dob;
+    }
 }
