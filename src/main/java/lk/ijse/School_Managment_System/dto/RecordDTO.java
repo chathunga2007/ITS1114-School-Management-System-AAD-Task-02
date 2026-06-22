950c6dc8b86da5ae5af106dcaaf245ae67c5fbc2
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-06-22 09:26:39 +0530

    record dto updated!

--- src/main/java/lk/ijse/School_Managment_System/dto/RecordDTO.java
+++ src/main/java/lk/ijse/School_Managment_System/dto/RecordDTO.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RecordDTO {
     private long recordId;
+    private LocalDate borrowDate;
     private long studentId;
     private long bookId;
 }
